f058923ba42f5452e3ab14a4360f6f8dc08fa776
Abdul Hafiz Ramadan <abdulhafizramadan.dev@gmail.com>
2024-06-29 21:39:52 +0700

    feat: add table view with default cell

--- UITableViewCustomCellStoryBoard/ViewController.swift
+++ UITableViewCustomCellStoryBoard/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    private let names = [
+        "Abdul",
+        "Hafiz",
+        "Ramadan",
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    
+    // Table Delegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row select at \(indexPath.row)")
+    }
+    
+    // Table DataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = names[indexPath.row]
+        
+        return cell
+    }
 }
 
